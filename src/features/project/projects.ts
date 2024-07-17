af5826af789f973e1df43fcfd4648a5b9193dd4d
norrisasd <norrishipolito@gmail.com>
2024-07-18 00:07:09 +0800

    feat: setup for projects/slug

--- src/features/project/projects.ts
+++ src/features/project/projects.ts
@@ -17,4 +17,30 @@ export type Project = {
   sourceLink?: string;
 };
 
-export const projects: Project[] = [];
+export const projects: Project[] = [
+  {
+    slug: "test",
+    title: "test",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc.", // 100 characters
+    projectImg: "https://via.placeholder.com/150",
+    href: "/test",
+    subTitle: "Sub Title",
+    icons: [
+      {
+        href: "https://flutter.dev/",
+        src: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/flutter-colored.svg",
+        alt: "Flutter",
+      },
+      {
+        href: "https://nextjs.org/",
+        src: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/nextjs-colored-dark.svg",
+        darkSrc:
+          "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/nextjs-colored.svg",
+        alt: "NextJs",
+      },
+    ],
+    liveLink: "https://flutter.dev/",
+    sourceLink: "https://flutter.dev/",
+  },
+];
